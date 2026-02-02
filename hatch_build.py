ad5bee38af04f78df69be444a554f5c70dd6d2ec
Joe Polny <joepolny@gmail.com>
2026-02-02 15:08:13 -0500

    ci: initial ci

---
+++ hatch_build.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface  # type: ignore
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        # Get the project root directory from the build data
+        # build_data contains the root directory of the project
+        project_root = self.root
+        rust_dir = os.path.join(project_root, "rust-ed25519-bip32")
+        
+        # Run cargo build --release
+        subprocess.run(
+            ["cargo", "build", "--release"],
+            cwd=rust_dir,
+            check=True
+        )
+        
+        return super().initialize(version, build_data)
